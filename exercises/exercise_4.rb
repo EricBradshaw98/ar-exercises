6f2994f83913886cb0e784513f3a6bd09484a047
Eric <ericbradshaw98@gmail.com>
2024-03-04 16:20:08 -0500

    exercise 4 done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+#- Richmond (annual_revenue of 1260000 carries women's apparel only)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+#- Gastown (annual_revenue of 190000 carries men's apparel only)
+
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "Stores that carry men's apparel:"
+@mens_stores.each { |store| puts "#{store.name} - Annual Revenue: #{store.annual_revenue}" }
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue > ?", true, 1000000)
+puts "Women's stores with annual revenue over 1 million:"
+@womens_stores.each { |store| puts "#{store.name} - Annual Revenue: #{store.annual_revenue}" }
